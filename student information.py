77d178592a4c7eff2d73a6d6cbfda6f1be6f604b
vibhor28sharma08 <sharmavibhor2808@gmail.com>
2024-07-03 23:12:32 +0530

    student information.py

--- student information.py
+++ student information.py
@@ -3,6 +3,9 @@ class student_info:
         self.name = name
         self.roll_no = roll_no
         self.course = course
+        name= input("enter the name:")
+        roll_no= int(input("enter the roll no:"))
+        course= input("enter the course:")
     
     def myfunc(self):
         print( self.name,self.roll_no,self.course)
